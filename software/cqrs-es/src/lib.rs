b1e5f2322d2452929283a5b072409cc404d42633
Hirokazu Nishioka <hiro@nisshiee.org>
2020-01-31 16:14:29 +0900

    now

--- software/cqrs-es/src/lib.rs
+++ software/cqrs-es/src/lib.rs
@@ -1,9 +1,12 @@
 extern crate failure;
 extern crate serde;
+#[cfg(test)]
+#[macro_use]
+extern crate failure_derive;
 
 use failure::Fail;
 use serde::de::DeserializeOwned;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use std::fmt::Debug;
 use std::hash::Hash;
 
@@ -14,6 +17,15 @@ pub trait Aggregate: Default {
 
     // `/` で区切ることで階層化することを想定
     fn type_name() -> &'static str;
+}
+
+#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Serialize, Deserialize)]
+pub struct Version(u64);
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct VersionedAggregate<A: Aggregate> {
+    version: Version,
+    aggregate: A,
 }
 
 pub trait AggregateId<A: Aggregate>:
@@ -85,6 +97,87 @@ pub trait Projector<A: Aggregate>: Debug {
 
 #[cfg(test)]
 mod tests {
+    use crate::{Aggregate, AggregateId, Command, CommandError, Event};
+    use serde::{Deserialize, Serialize};
+
+    struct TestAggregate(u64);
+
+    impl Default for TestAggregate {
+        fn default() -> Self {
+            TestAggregate(0)
+        }
+    }
+
+    #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+    struct TestAggregateId(u64);
+
+    impl ToString for TestAggregateId {
+        fn to_string(&self) -> String {
+            format!("{}", self.0)
+        }
+    }
+
+    impl AggregateId<TestAggregate> for TestAggregateId {}
+
+    #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
+    enum TestEvent {
+        Increased,
+    }
+
+    impl Event<TestAggregate> for TestEvent {
+        fn apply_to(self, aggregate: &mut TestAggregate) {
+            aggregate.0 = aggregate.0 + 1
+        }
+    }
+
+    enum TestCommand {
+        Increase,
+    }
+
+    #[derive(Fail, Debug)]
+    #[fail(display = "unknown")]
+    struct TestCommandError {}
+
+    impl CommandError for TestCommandError {}
+
+    impl Command<TestAggregate> for TestCommand {
+        type Events = Option<TestEvent>;
+        type Error = TestCommandError;
+
+        fn execute_on(self, _aggregate: &TestAggregate) -> Result<Self::Events, Self::Error> {
+            Ok(Some(TestEvent::Increased))
+        }
+    }
+
+    impl Aggregate for TestAggregate {
+        type Id = TestAggregateId;
+        type Event = TestEvent;
+        type Command = TestCommand;
+
+        fn type_name() -> &'static str {
+            "test"
+        }
+    }
+
+    #[test]
+    fn event() {
+        let event = TestEvent::Increased;
+        let mut aggregate = TestAggregate(0);
+        event.apply_to(&mut aggregate);
+        assert_eq!(aggregate.0, 1);
+    }
+
+    #[test]
+    fn command() {
+        let command = TestCommand::Increase;
+        let aggregate = TestAggregate(0);
+        if let Ok(Some(got)) = command.execute_on(&aggregate) {
+            assert_eq!(got, TestEvent::Increased);
+        } else {
+            assert!(false)
+        }
+    }
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
